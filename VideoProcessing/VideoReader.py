a782ecc4042bb66a4eb3bbbf7fd44fb998efda86
Augustas Macys <axm1415@cs.bham.ac.uk>
2020-11-03 19:33:13 +0000

    Video Frame Extractor Working

--- VideoProcessing/VideoReader.py
+++ VideoProcessing/VideoReader.py
@@ -13,17 +13,19 @@ from utilities import ROOT_DIR
 
 def video_frame_extractor(video_name):
     capturator = cv2.VideoCapture(video_name)
-    frames_number = capturator.get(cv2.CAP_PROP_FRAME_COUNT)
+    frames_number = int(capturator.get(cv2.CAP_PROP_FRAME_COUNT))
 
     for i in range(frames_number):
         capturator.grab()
-        if i % 5 == 0:
+        if i % 10 == 0:
             result, frame = capturator.retrieve()
             if not result:
                 continue
 
-        identifier = ntpath.basename(video_name) + '_' + str(i)
-        cv2.imwrite(os.path.join(ROOT_DIR, "extracted_images", identifier), [cv2.IMWRITE_JPEG_QUALITY, 100])
+            identifier = ntpath.basename(video_name)[:-4] + '_' + str(i)
+            save_string = os.path.join(ROOT_DIR, "extracted_images", identifier) + ".jpeg"
+            cv2.imwrite(save_string,
+                        frame, params=[cv2.IMWRITE_JPEG_QUALITY, 100])
 
     capturator.release()
 
@@ -36,7 +38,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     full_video_path = os.path.join(ROOT_DIR, args.videoFolder)
-    video_filenames = [path for path in glob.glob(os.path.join(full_video_path, "*/*.mp4"))]
+
+    video_filenames = [os.path.join(full_video_path, path) for path in
+                       os.listdir(full_video_path) if path.endswith('.mp4')]
     with Pool(processes=os.cpu_count() - 2) as pool:
         with tqdm(total=len(video_filenames)) as bar:
             for video in pool.imap_unordered(partial(video_frame_extractor), video_filenames):
--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import json
 import os
+import shutil
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 METADATA_FILENAME = "metadata.json"
@@ -22,8 +23,26 @@ def get_specific_video_names(folder, number=20, label='FAKE'):
 
 
 def copy_specific_videos(src_folder, src_names, destination_folder):
-    
+    for name in src_names:
+        path = os.path.join(src_folder, name)
+        destination_path = os.path.join(destination_folder, name)
+        shutil.copyfile(path, destination_path)
 
 
-folder00 = r"D:\deepfakes\data\train\train_00\dfdc_train_part_0"
-print(get_specific_video_names(folder00))
+def write_list_to_file(list, dest, label):
+    path = os.path.join(dest, label)
+    with open(path, 'w') as f:
+        for name in list:
+            f.write("%s\n" % name)
+
+
+# destination_folder = r"D:\deepfakes\video_examples"
+# folder00 = r"D:\deepfakes\data\train\train_00\dfdc_train_part_0"
+# fakes = get_specific_video_names(folder00)
+# real = get_specific_video_names(folder00, label="REAL")
+#
+# write_list_to_file(fakes, destination_folder, "FAKES.txt")
+# write_list_to_file(real, destination_folder, "REAL.txt")
+#
+# copy_specific_videos(folder00, fakes, destination_folder)
+# copy_specific_videos(folder00, real, destination_folder)
