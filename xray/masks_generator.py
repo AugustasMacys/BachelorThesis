823cd7645951e70730334a107808256c8e76da08
Augustas Macys <axm1415@cs.bham.ac.uk>
2021-01-25 19:42:13 +0000

    Started to create dataset for X-Ray

--- xray/masks_generator.py
+++ xray/masks_generator.py
@@ -5,13 +5,13 @@ from scipy.spatial import ConvexHull
 import face_alignment
 import pandas as pd
 from tqdm import tqdm
+import os
 
 
-from utilities import PAIR_UPDATED_FAKE_DATAFRAME, PAIR_UPDATED_FAKE_DATAFRAME2, PAIR_UPDATED_FAKE_DATAFRAME3,\
-    MASKS_FOLDER
+from utilities import MASKS_FOLDER, PAIR_UPDATED_REAL_DATAFRAME
 
 
-def mask_creator(path):
+def create_mask(path):
     img = Image.open(path)
     img = np.asarray(img)
     height, width, _ = img.shape
@@ -21,29 +21,28 @@ def mask_creator(path):
         return
     points = preds[0]
     hull = ConvexHull(points)
-    img = cv2.drawContours(black_mask, [points[hull.vertices].astype(int)], -1, (255, 255, 255), thickness=cv2.FILLED)
-    cv2.imwrite(r"D:/deepfakes/x_ray_example_on_one_picture/convex_hull_black_frame.png", img)
+    del preds
+    img = cv2.drawContours(black_mask, [points[hull.vertices].astype(int)], -1, (1, 1, 1), thickness=cv2.FILLED)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = 4.0 * np.multiply(img, (1.0 - img))
+    img = (img * 255).astype(int)
+    folder = path.split("\\")[3]
+    identifier = path.split("\\")[4]
+    directory_to_create = os.path.join(MASKS_FOLDER, folder)
+    if not os.path.exists(directory_to_create):
+        os.mkdir(directory_to_create)
+
+    save_string = os.path.join(directory_to_create, identifier)
+    cv2.imwrite(save_string, img)
 
 
 if __name__ == '__main__':
     fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
 
-    # Masks are needed only for fake faces as real faces mask is just black
-    image_paths = pd.read_csv(PAIR_UPDATED_FAKE_DATAFRAME).image_path
+    # According to research paper, we create masks from real images
+    image_paths = pd.read_csv(PAIR_UPDATED_REAL_DATAFRAME).image_path
 
     with tqdm(total=len(image_paths)) as bar:
         for path in image_paths:
-            mask_creator(path)
-            bar.update()
-
-    image_paths = pd.read_csv(PAIR_UPDATED_FAKE_DATAFRAME2).image_path
-    with tqdm(total=len(image_paths)) as bar:
-        for path in image_paths:
-            mask_creator(path)
-            bar.update()
-
-    image_paths = pd.read_csv(PAIR_UPDATED_FAKE_DATAFRAME3).image_path
-    with tqdm(total=len(image_paths)) as bar:
-        for path in image_paths:
-            mask_creator(path)
+            create_mask(path)
             bar.update()
