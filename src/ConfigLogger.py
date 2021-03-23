8b98c4ae5aa233b08644176dbf7b63e583d39e56
Augustas Macys <axm1415@cs.bham.ac.uk>
2021-03-23 22:06:09 +0000

    logger

--- src/ConfigLogger.py
+++ src/ConfigLogger.py
@@ -1,7 +1,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-handler = RotatingFileHandler(filename='../logs/coviar_combined_testing.log', maxBytes=20000000, backupCount=10)
+handler = RotatingFileHandler(filename='../logs/new_training_xray.log', maxBytes=20000000, backupCount=10)
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s, %(name)s, %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
