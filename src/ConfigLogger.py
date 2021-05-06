fa3cc678281706bc79ab5d89484fd4e71f2471fa
Augustas Macys <axm1415@cs.bham.ac.uk>
2021-05-06 18:14:58 +0100

    Ready for Merging

--- src/ConfigLogger.py
+++ src/ConfigLogger.py
@@ -1,7 +1,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-handler = RotatingFileHandler(filename='../../logs/Log_Training.log', maxBytes=20000000, backupCount=10)
+handler = RotatingFileHandler(filename='../../logs/Demo_log.log', maxBytes=20000000, backupCount=10)
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s, %(name)s, %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
